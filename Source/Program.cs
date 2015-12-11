65e37872a8ecd47a4a57c6c7aab36125d9e249b6
Peter <peter@ecomerc.com>
2015-12-11 12:39:56 +0100

    merging and cleanup

--- Source/Program.cs
+++ Source/Program.cs
@@ -13,43 +13,43 @@ using System.Diagnostics;
 namespace xscreenshot {
     class Program {
 
-		private static void TryLoadConfig(string file) {
-			try {
-				if (File.Exists(file) && (file.EndsWith(".conf") || file.EndsWith(".config"))) {
-				var fileInfo = new FileInfo(args[0]);
-					Console.WriteLine("Loading config: " + fileInfo.FullName);
-					var config = JsonConfig.Config.ApplyJsonFromPath(fileInfo.FullName);
-				JsonConfig.Config.SetUserConfig(config);
-				}
-					} catch (Exception ex) {
-						Console.WriteLine(string.Format("Supplied config file was invalid: {0}", ex.Message));
-					}
-		}
+        private static void TryLoadConfig(string file) {
+            try {
+                if (File.Exists(file) && (file.EndsWith(".conf") || file.EndsWith(".config") || file.EndsWith(".json"))) {
+                    var fileInfo = new FileInfo(file);
+                    Console.WriteLine("Loading config: " + fileInfo.FullName);
+                    var config = JsonConfig.Config.ApplyJsonFromPath(fileInfo.FullName);
+                    JsonConfig.Config.SetUserConfig(config);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(string.Format("Supplied config file was invalid: {0}", ex.Message));
+            }
+        }
 
-		private static void TryLoadScript(string file) {
-			try {
-				if (File.Exists(file) && (file.EndsWith(".cs") || file.EndsWith(".script"))) {
-					var fileInfo = new FileInfo(args[0]);
-					Console.WriteLine("Loading script: " + fileInfo.FullName);
-					var config = JsonConfig.Config.ApplyJsonFromPath(fileInfo.FullName);
-					JsonConfig.Config.SetUserConfig(config);
-				}
-			} catch (Exception ex) {
-				Console.WriteLine(string.Format("Supplied script file was invalid: {0}", ex.Message));
-			}
-		}
+        private static void TryLoadScript(string file) {
+            try {
+                if (File.Exists(file) && (file.EndsWith(".cs") || file.EndsWith(".script"))) {
+                    var fileInfo = new FileInfo(file);
+                    Console.WriteLine("Loading script: " + fileInfo.FullName);
+                    var config = JsonConfig.Config.ApplyJsonFromPath(fileInfo.FullName);
+                    JsonConfig.Config.SetUserConfig(config);
+                }
+            } catch (Exception ex) {
+                Console.WriteLine(string.Format("Supplied script file was invalid: {0}", ex.Message));
+            }
+        }
 
         static void Main(string[] args) {
 
             if (args.Count() > 0 && File.Exists(args[0])) {
-				TryLoadConfig (args [0]);
-				TryLoadScript (args [0]);
+                TryLoadConfig(args[0]);
+                TryLoadScript(args[0]);
             }
 
-			if (args.Count() > 1 && File.Exists(args[1])) {
-				TryLoadConfig (args [1]);
-				TryLoadScript (args [1]);
-			}
+            if (args.Count() > 1 && File.Exists(args[1])) {
+                TryLoadConfig(args[1]);
+                TryLoadScript(args[1]);
+            }
 
 
             if (Directory.Exists(Config.Global.iOS.DevicesPath)) {
@@ -66,33 +66,34 @@ namespace xscreenshot {
                     includeDevices = Config.Global.iOS.IncludeDevices;
                 }
 
-				Process.Start (new ProcessStartInfo ("killall \"iOS Simulator\")") {UseShellExecute = false});
-				Process.Start (new ProcessStartInfo ("killall \"Simulator\")") {UseShellExecute = false});
+                Process.Start(new ProcessStartInfo("killall \"iOS Simulator\")") { UseShellExecute = false });
+                Process.Start(new ProcessStartInfo("killall \"Simulator\")") { UseShellExecute = false });
 
                 var devices = SimulatorHelpers.LoadSimulators(path, includeDevices, excludedDevices, Config.Global.iOS.OSVersion);
 
 
                 foreach (Simulator device in devices) {
-					Console.WriteLine(string.Format("Beginning screenshots for device: {0} {1} {2}", device.Name, device.UDID, device.iOSVersion));
-					screenShotIndex = 0;
+                    Console.WriteLine(string.Format("Beginning screenshots for device: {0} {1} {2}", device.Name, device.UDID, device.iOSVersion));
+                    screenShotIndex = 0;
 
 
-					Process.Start (new ProcessStartInfo ("xcrun simctl erase #{aDeviceId}") {UseShellExecute = false});
+                    Process.Start(new ProcessStartInfo(string.Format("xcrun simctl erase {0}", device.UDID)) { UseShellExecute = false });
 
-					string outputPath = Path.Combine(Config.Global.iOS.OutputPath, device.Name + "." + device.iOSVersion);
+                    string outputPath = Path.Combine(Config.Global.iOS.OutputPath, device.Name + "." + device.iOSVersion);
 
-					Directory.CreateDirectory (Config.Global.iOS.OutputPath);
+                    Directory.CreateDirectory(Config.Global.iOS.OutputPath);
 
                     try {
-						using (var app = Xamarin.UITest.ConfigureApp.iOS.EnableLocalScreenshots()
-                                .AppBundle((string)Config.Global.iOS.AppPath)
-                                .DeviceIdentifier(device.UDID)
-							.StartApp()) {
 
-	                 
 
-							app.Back();
-						}
+                        var app = Xamarin.UITest.ConfigureApp.iOS.EnableLocalScreenshots()
+                               .AppBundle((string)Config.Global.iOS.AppPath)
+                               .DeviceIdentifier(device.UDID)
+                           .StartApp();
+
+
+                        app.Back();
+                        app = null;
 
                     } catch (Exception ex) {
                         Console.WriteLine(string.Format("Could not run test: {0}", ex.Message));
@@ -108,8 +109,8 @@ namespace xscreenshot {
             Console.WriteLine("All done");
 
 
-			Process.Start (new ProcessStartInfo ("killall \"iOS Simulator\")") {UseShellExecute = false});
-			Process.Start (new ProcessStartInfo ("killall \"Simulator\")") {UseShellExecute = false});
+            Process.Start(new ProcessStartInfo("killall \"iOS Simulator\")") { UseShellExecute = false });
+            Process.Start(new ProcessStartInfo("killall \"Simulator\")") { UseShellExecute = false });
 
         }
 
@@ -119,10 +120,10 @@ namespace xscreenshot {
             try {
                 screenShotIndex++;
                 var fi = app.Screenshot(title);
-				var filename = path + "." + screenShotIndex.ToString() + ".png";
-				Console.WriteLine(filename);
-					
-				fi.MoveTo(filename);
+                var filename = path + "." + screenShotIndex.ToString() + ".png";
+                Console.WriteLine(filename);
+
+                fi.MoveTo(filename);
             } catch (Exception ex) {
                 Console.WriteLine(string.Format("Could not capture screenshot: {0}", ex.Message));
             }
@@ -146,8 +147,8 @@ namespace xscreenshot {
             CompilerResults r = csc.CompileAssemblyFromSource(p, "using System; class p {public static object c(){" + code + "}}");
             if (r.Errors.Count > 0) { r.Errors.Cast<CompilerError>().ToList().ForEach(error => Console.WriteLine(error.ErrorText)); return null; }
             System.Reflection.Assembly a = r.CompiledAssembly;
-			var 
-            MethodInfo o = a.CreateInstance("p").GetType().GetMethod("c");
+            var instance = a.CreateInstance("p");
+            MethodInfo o = .GetType().GetMethod("c");
             return o.Invoke(o, null);
         }
     }
